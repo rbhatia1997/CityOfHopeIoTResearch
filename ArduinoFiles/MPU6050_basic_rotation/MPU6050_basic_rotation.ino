02ecdf9813d902a6886f372d4d5eee61c39d4cfe
Josephine King <pking@g.hmc.edu>
2018-11-01 20:35:29 -0700

    basic rotation

---
+++ ArduinoFiles/MPU6050_basic_rotation/MPU6050_basic_rotation.ino
@@ -0,0 +1,91 @@
+
+
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
+int avg_size = 7;
+
+double z_yaw = 0;
+double y_pitch = 0;
+double x_roll = 0;
+
+double x_avg = 0;
+double y_avg = 0;
+double z_avg = 0;
+
+double x_avg_past = 0;
+double y_avg_past = 0;
+double z_avg_past = 0;
+
+double x_offset = 0;
+double y_offset = 0;
+double z_offset = 0;
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+  start_time = millis();
+  
+}
+
+void loop() {
+  
+  mpu6050.update();
+  // Get data from IMU
+  x_roll = mpu6050.getAngleX();
+  y_pitch = mpu6050.getAngleY();
+  z_yaw = mpu6050.getAngleZ();
+
+  // Calculated offsets in the beginning, so t < 7s
+  if (millis() - start_time < 7000) {
+    
+    // Calculate offsets
+    x_offset = x_roll;
+    y_offset = y_pitch;
+    z_offset = z_yaw;
+    
+    // Print offsets
+    Serial.print("------ OFFSETS -----");
+    Serial.print(x_offset);
+    Serial.print("  ");
+    Serial.print(y_offset);
+    Serial.print("  ");
+    Serial.println(z_offset);
+    
+  }
+
+  // Calculate the calibrated angles by subtracting the intial offsets
+  x_roll = x_roll - x_offset;
+  y_roll = y_roll - y_offset;
+  z_roll = z_roll - z_offset;
+
+  // Filter signal using moving average
+  double x_avg = ( (avg_size - 1)*x_avg_past + x_roll) / avg_size - x_cal;
+  double y_avg = ( (avg_size - 1)*y_avg_past + y_pitch) / avg_size - y_cal;
+  double z_avg = ( (avg_size - 1)*z_avg_past + z_yaw) / avg_size - z_cal;
+  x_avg_past = x_avg;
+  y_avg_past = y_avg;
+  z_avg_past = z_avg;
+
+  // Print the unfiltered (but calibrated) angles
+  Serial.print(x_roll);
+  Serial.print("  ");
+  Serial.print(y_pitch);
+  Serial.print("  ");
+  Serial.println(z_yaw);
+  Serial.print("  ");
+  
+  // Print the filtered, calibrated angles
+  Serial.print(x_avg);
+  Serial.print("  ");
+  Serial.print(y_avg);
+  Serial.print("  ");
+  Serial.println(z_avg);
+  
+}

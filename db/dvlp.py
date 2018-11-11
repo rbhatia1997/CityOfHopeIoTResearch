a4056d1c06f7812735a6c23f0bb43542b0f39001
Darien Joso <darienjoso@WL-218-95.WPA.Claremont.Edu>
2018-11-11 02:56:50 -0800

    working on the api

--- db/dvlp.py
+++ db/dvlp.py
@@ -1,10 +1,10 @@
 import psycopg2
 
 try:
-    conn=psycopg2.connect("host='coh-test-db.ccotkfp86qay.us-east-2.rds.amazonaws.com' port='5432' dbname='coh_dvlp' user='Fall2018COH' password='cityofhope'")
+    conn=psycopg2.connect("host='coh-test-db.ccotkfp86qay.us-east-2.rds.amazonaws.com' port='5676' dbname='coh_dvlp' user='Fall2018COH' password='cityofhope'")
 except:
     print("I am unable to connect to the database.")
-    
+
 cur = conn.cursor()
 
 try:
@@ -14,7 +14,7 @@ except:
 
 cur.execute( "CREATE TABLE johnlee (bip_count INTEGER, yeet_frequency INTEGER, is_he_a_ho VARCHAR(255), whose_fault_is_that VARCHAR(255));" )
 
-cur.execute( "INSERT INTO johnlee (bip_count, yeet_frequency, is_he_a_ho, whose_fault_is_that) VALUES(732, 2, 'Of course', 'John');" )
+# cur.execute( "INSERT INTO johnlee (bip_count, yeet_frequency, is_he_a_ho, whose_fault_is_that) VALUES(732, 2, 'Of course', 'John');" )
 
 conn.commit()
 

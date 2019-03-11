fa79e4a42aad7dd84479b7a1c9c0a1a1d81c1a90
rbhatia <rbhatia@hmc.edu>
2019-03-10 20:24:22 -0700

    Made minor modifications to the source code for the Arduino files

--- SP19/Bluetooth/RonakWork/BLE_RecongnizedbyiPhone_Version1.01/BLE_RecongnizedbyiPhone_Version1.01.ino
+++ SP19/Bluetooth/RonakWork/BLE_RecongnizedbyiPhone_Version1.01/BLE_RecongnizedbyiPhone_Version1.01.ino
@@ -1,8 +1,12 @@
 /*
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updates by chegewara
+    Based on the Arduino ESP32 Port by Evandro Copercini & Chegewara
+    General changes & adaptation for iOS by Ronak Bhatia & Darien Joso from City of Hope's HMC Clinic
+    Last Date Updated: Sunday, March 10, 2019
 */
+
+// The following are libraries necessary for enabling BLE connection
+// They are in-built into the ESP-32 Library and wouldn't need one to manually install.
 
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -11,7 +15,32 @@
 #include <iostream>
 #include <string>
 
-BLECharacteristic *pCharacteristic;
+/*
+
+   The BLE Peripheral is typically the board you are programming. It connects the BLE central to
+   expose its characteristics. In our case, the iPhone is the BLE central, which is the device that
+   asks the BLE peripheral its data. That's why we won't be using a BLECentral constructor.
+
+   Characteristics contain at least two attributes: (1) characteristic declaration, which has metadata
+   about the data, and (2) the characteristic value, which contains the data itself.
+
+   Characteristics have names, UUIDs, values, and read/write/notify properties. There are many types
+   of characteristics constructors depending on the data type wanting to be used.
+
+   The purpose of this code is to create a Bluetooth (BLE) Server, which does the following steps:
+   (1) Create a BLE Server
+   (2) Create a BLE Service
+   (3) Create a BLE Characteristic based on this service
+   (4) Create a BLE Descriptor based on the characteristic
+   (5) Start the service and then start advertising
+
+   In other words, a GATT server creates a service, which can contain multiple characteristics that
+   include metadata, which in our case will be the data from the IMU (Quaternion or otherwise).
+
+   A Quaternion is a four-element vector used to encode any rotation in a 3D coordinate system.
+*/
+
+BLECharacteristic *pCharacteristic; // define global variable - characteristic
 bool deviceConnected = false;
 int humidity = 0;
 int temperature = 0;
@@ -20,8 +49,18 @@ int temperature = 0;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
+// Important to note that normally one would set up a UUID_TX and UUID_RX
+// However, because we only want to transmit data not receive any from the iPhone,
+// we just need one Characteristic UUID for TX, which we call Characteristic UUID.
+
 #define SERVICE_UUID        "2f391f0f-1c30-46fb-a972-a22c2f7570ee"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// This is a callback function that handles the Bluetooth Connection status.
+// If the device is recognized, have the ESP32 change its connection status.
+// Because we aren't receiving data sent by a client, we don't include a
+// Callback function for receiving data - which would be standard.
+
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -34,15 +73,22 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  Serial.begin(115200); // this BAUD rate is set intentionally
+  Serial.println("Initialzing ESP32 as BLE Device...");
 
   BLEDevice::init("ESP32_Bluetooth_Testing"); // designating this as a test.
   BLEServer *pServer = BLEDevice::createServer(); // creates server
 
   pServer->setCallbacks(new MyServerCallbacks());
 
+  Serial.println("Creating a BLE Server...");
+
+
+  // Create BLE service using the service UUID.
   BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Add characteristics...
+  // PROPERTY_WRITE, PROPERTY_NOTIFY, PROPERTY_WRITE are the properties of the characteristics.
 
   pCharacteristic = pService->createCharacteristic(
                       CHARACTERISTIC_UUID,
@@ -88,5 +134,5 @@ void loop() {
   Serial.print("*** Dado enviado: ");
   Serial.print(dhtDataString);
   Serial.println(" ***");
-  delay(1000);
+  delay(2000);
 }

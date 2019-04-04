38174816ac7d0589adbfa1024f6eefee81d608c7
s3chandra <schandra@g.hmc.edu>
2019-04-03 22:57:39 -0700

    SC April 3; first pass at UART between Teensy and ESP

--- SP19/COH_Main_Library/Bluetooth.cpp
+++ SP19/COH_Main_Library/Bluetooth.cpp
@@ -2,13 +2,32 @@
 
 
 Bluetooth::Bluetooth(void){
-    
+
 }
 
 void Bluetooth::init(void){
-    
+  HWSERIAL.begin(baudRate);
+  COMPSERIAL.begin(baudRate);
 }
 
+//allows float to byte change
+//doesn't need to be public
+union{
+  float fval;
+  byte bval[4];
+}floatAsByte;
 
+void float2Byte(float value)
+{
+  floatAsByte.fval = value;
+}
 
-
+void sendData()
+{
+  COMPSERIAL.write("Sending Data...");
+  for(int i = 0; i<3; i++)
+  {
+    HWSERIAL.write(floatAsByte.bval[i]);
+    COMPSERIAL.write("sent + String(i)");
+  }
+}
